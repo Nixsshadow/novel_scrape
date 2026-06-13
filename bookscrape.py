e1e5d7841788a8526a20f0424cded088863f31f7
Nixsshadow <kyalbeip@gmail.com>
2026-06-13 13:47:56 +0300

    add pandas and save to csv or xlsx functionality

--- bookscrape.py
+++ bookscrape.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
 url = "http://books.toscrape.com/index.html" 
 
 response = requests.get(url) 
@@ -13,7 +13,7 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 cards = soup.select(".product_pod")
 print(cards [0]) 
-all_books = []
+all_scrapped_books = []
 for card in cards:
     title = card.select_one("h3 a").text
     print( title)
@@ -22,11 +22,14 @@ for card in cards:
      
     book_item = {
         "title": title,
-        "price": price
+        "price": price.replace("£", ""),
      }
-    all_books.append(book_item)
+    all_scrapped_books.append(book_item)
 
-print(all_books)
+# print(all_scrapped_books)    
 
+df = pd.DataFrame(all_scrapped_books)
+#df.to_csv("scrapped_books.csv")
+df.to_excel("scrapped_books.xlsx")
 
     
